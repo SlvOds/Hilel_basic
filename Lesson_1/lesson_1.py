fc97070476c4fe585574fcd0bf453c90745ab85a
ximera300028@gmail.com <ximera300028@gmail.com>
2023-07-06 20:34:55 +0300

    lesson 1

---
+++ Lesson_1/lesson_1.py
@@ -0,0 +1,47 @@
+#import antigravity
+# виводить комікс
+
+#import this
+# виводить принципи естетичного написання коду
+
+#import __hello__
+# виводить відоме привітання
+
+#from __future__ import braces
+# виводить логічну помилку
+
+
+
+#2
+
+from art import *
+name = input("Enter your name")
+name_user = text2art(name, font="starwars")
+print(name_user)
+
+#3
+
+print("Escape sequences\n")
+print("\\a Bell (alert)")
+print("\\b Backspace")
+print("\\New Line")
+print("Horizontal Tab")
+print("\\\\ Backslash \\")
+print("\\\" Double quotation mark\"")
+print("\\'Single quotation mark'")
+
+#4
+
+x = int(input("Enter the number x= "))
+y = int(input("Enter the number y= "))
+
+print(x, y)
+
+print(x + y)
+
+print(x // y)
+
+print(x % y)
+
+print(x ** y)
+
